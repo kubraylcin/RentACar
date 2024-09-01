0c13ab51dd67394360c1aa92b9c5c7561290579a
kubraylcn <kubrayalcin.272@gmail.com>
2024-09-02 01:25:15 +0300

    + Coding the Northwind Context Object

---
+++ DataAccess/Concrete/EntityFramework/Context/NorthwindContext.cs
@@ -0,0 +1,27 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class NorthwindContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=RentACarDBB;Trusted_Connection=true");
+        }
+
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        
+
+    }
+}
