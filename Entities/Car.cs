399c83971c6a015ef82318c16cb2ccbd65f8a0fd
kubraylcn <kubrayalcin.272@gmail.com>
2024-09-01 23:56:45 +0300

    + Entity Layer Coded

---
+++ Entities/Car.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Car:IEntity
+    {
+        public int CarId { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal DailyRate { get; set; }
+        public bool IsAvailable { get; set; }
+        public int DailyPrice { get; set; }
+    }
+}
---
+++ Entities/Payment.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Payment:IEntity
+    {
+        public int PaymentId { get; set; }
+        public int CustomerId { get; set; }
+        public int RentalId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+    }
+}
