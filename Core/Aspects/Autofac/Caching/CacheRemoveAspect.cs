c0436120099410f2adae52301783a0a773c31a55
kubraylcn <kubrayalcin.272@gmail.com>
2024-10-30 00:07:33 +0300

    Implement AOP features for caching and performance tracking

---
+++ Core/Aspects/Autofac/Caching/CacheRemoveAspect.cs
@@ -0,0 +1,24 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Caching;
+using Core.Utilities.Interceptors;
+using Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Aspects.Autofac.Caching
+{
+    public class CacheRemoveAspect(string pattern) : MethodInterception
+    {
+        private readonly string _pattern = pattern;
+        private readonly ICacheManager _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+
+        protected override void OnSuccess(IInvocation invocation)
+        {
+            _cacheManager.RemoveByPattern(_pattern);
+        }
+    }
+}
