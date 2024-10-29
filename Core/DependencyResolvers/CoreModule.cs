579c68f8ee4d462a51847271e34858559d608697
kubraylcn <kubrayalcin.272@gmail.com>
2024-10-30 00:05:41 +0300

    Configure dependency injection for core services

---
+++ Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,26 @@
+﻿using Core.CrossCuttingConcerns.Caching.Redis;
+using Core.CrossCuttingConcerns.Caching;
+using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DependencyResolvers
+{
+    public class CoreModule : ICoreModule
+    {
+        public void Load(IServiceCollection services)
+        {
+            services.AddStackExchangeRedisCache(options => options.Configuration = "localhost:6379");
+            services.AddSingleton<ICacheManager, RedisCacheManager>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<Stopwatch>();
+        }
+    }
+}
